97f9d9ac7371106252a658aae0c67052de9b01f7
William Martinez <williamr1994@gmail.com>
2022-11-12 23:18:43 -0600

    agregando crud de vuelos

--- Modulos/VueloPage.xaml.cs
+++ Modulos/VueloPage.xaml.cs
@@ -29,7 +29,7 @@ namespace ProyectoPOE.Modulos
     public partial class VuelosPage : Page
     {
         public int EstadoActual;
-        public int utlimoId;
+        public int ultimoId;
         public VuelosPage()
         {
             InitializeComponent();
@@ -38,40 +38,52 @@ namespace ProyectoPOE.Modulos
 
         private void Refresh()
         {
-            List<VueloViewModel> listPasajero = new List<VueloViewModel>();
+            List<VueloViewModel> listVuelos = new List<VueloViewModel>();
             using (Modelo.sivarviajesEntities db = new Modelo.sivarviajesEntities())
             {
-                listPasajero = (from d in db.pasajero
+                listVuelos = (from v in db.vuelo
                                   select new VueloViewModel
                                   {
-                                      id = d.idPasajero,
-                                      nombre = d.nombre,
-                                      direccion = d.direccion,
-                                      telefono = d.telefono,
-                                      email = d.email
+                                      id = v.idVuelo,
+                                      aerolinea = v.aerolinea,
+                                      origen = v.origen,
+                                      destino = v.destino,
+                                      fecha = v.fecha,
+                                      salida = v.hora_salida,
+                                      llegada = v.hora_llegada,
+                                      asientos = v.asientos,
+                                      costo = v.costoVuelo
                                   }
                                   ).ToList();
             }
 
-            DGPasajero.ItemsSource = listPasajero;
-            if (listPasajero.Count == 0)
-                utlimoId = 0;
+            DGVuelos.ItemsSource = listVuelos;
+            if (listVuelos.Count == 0)
+                ultimoId = 0;
             else
-                utlimoId = listPasajero.Last().id;
+                ultimoId = listVuelos.Last().id;
 
-            txtDireccion.IsEnabled = false;
-            txtNombre.IsEnabled = false;
-            txtTelefono.IsEnabled = false;
+            txtAerolinea.IsEnabled = false;
+            txtOrigen.IsEnabled = false;
+            txtDestino.IsEnabled = false;
+            dpFecha.IsEnabled = false;
+            txtSalida.IsEnabled = false;
+            txtLlegada.IsEnabled = false;
+            txtAsientos.IsEnabled = false;
+            txtCosto.IsEnabled = false;
 
-            txtEmail.IsEnabled = false;
             btnGuardar.IsEnabled = false;
 
             chkEliminar.IsChecked = false;
             Agregar_nuevo.IsEnabled = true;
 
-            txtDireccion.Text = "";
-            txtNombre.Text = "";
-            txtTelefono.Text = "";
+            txtAerolinea.Text = "";
+            txtOrigen.Text = "";
+            txtDestino.Text = "";
+            txtSalida.Text = "";
+            txtLlegada.Text = "";
+            txtAsientos.Text = "";
+            txtCosto.Text = "";
             EstadoActual = Convert.ToInt32(EstadosVuelo.Default);
 
         }
@@ -84,30 +96,38 @@ namespace ProyectoPOE.Modulos
                 if (EstadoActual == ((int)EstadosVuelo.Nuevo))
                 {
                     //string FechaFormat;
-                    var oPasajero = new Modelo.pasajero();
-                    oPasajero.nombre = txtNombre.Text;
-                    oPasajero.telefono = txtTelefono.Text;
-                    oPasajero.email = txtEmail.Text;
-                    oPasajero.direccion = txtDireccion.Text;
-                    db.pasajero.Add(oPasajero);
-                    db.Entry(oPasajero).State = System.Data.Entity.EntityState.Added;
+                    var oVuelo = new Modelo.vuelo();
+                    oVuelo.aerolinea = txtAerolinea.Text;
+                    oVuelo.origen = txtOrigen.Text;
+                    oVuelo.destino = txtDestino.Text;
+                    oVuelo.fecha = dpFecha.SelectedDate;
+                    oVuelo.hora_salida = TimeSpan.Parse(txtSalida.Text);
+                    oVuelo.hora_llegada = TimeSpan.Parse(txtLlegada.Text);
+                    oVuelo.asientos = int.Parse(txtAsientos.Text);
+                    oVuelo.costoVuelo = Double.Parse(txtCosto.Text);
+                    db.vuelo.Add(oVuelo);
+                    db.Entry(oVuelo).State = System.Data.Entity.EntityState.Added;
                     
                     db.SaveChanges();
                 }
                 else if (EstadoActual == ((int)EstadosVuelo.Modificar))
                 {
-                    var oPasajero = db.pasajero.Find(Convert.ToInt32(txtIdPropietrio.Text));
-                    oPasajero.nombre = txtNombre.Text;
-                    oPasajero.telefono = txtTelefono.Text;
-                    oPasajero.direccion = txtDireccion.Text;
-                    oPasajero.email = txtEmail.Text;
-                    db.Entry(oPasajero).State = System.Data.Entity.EntityState.Modified;
+                    var oVuelo = db.vuelo.Find(Convert.ToInt32(txtIdVuelo.Text));
+                    oVuelo.aerolinea = txtAerolinea.Text;
+                    oVuelo.origen = txtOrigen.Text;
+                    oVuelo.destino = txtDestino.Text;
+                    oVuelo.fecha = dpFecha.SelectedDate;
+                    oVuelo.hora_salida = TimeSpan.Parse(txtSalida.Text);
+                    oVuelo.hora_llegada = TimeSpan.Parse(txtLlegada.Text);
+                    oVuelo.asientos = int.Parse(txtAsientos.Text);
+                    oVuelo.costoVuelo = Double.Parse(txtCosto.Text);
+                    db.Entry(oVuelo).State = System.Data.Entity.EntityState.Modified;
                     db.SaveChanges();
                 }
                 else if (EstadoActual == ((int)EstadosVuelo.Eliminar) || chkEliminar.IsChecked == true)
                 {
-                    var oPasajero = db.pasajero.Find(Convert.ToInt32(txtIdPropietrio.Text));
-                    db.Entry(oPasajero).State = System.Data.Entity.EntityState.Deleted;
+                    var oVuelo = db.vuelo.Find(Convert.ToInt32(txtIdVuelo.Text));
+                    db.Entry(oVuelo).State = System.Data.Entity.EntityState.Deleted;
                     db.SaveChanges();
                 }
 
@@ -118,10 +138,14 @@ namespace ProyectoPOE.Modulos
         private void Agregar_nuevo_Click(object sender, RoutedEventArgs e)
         {
             EstadoActual = Convert.ToInt32(EstadosVuelo.Nuevo);
-            txtDireccion.IsEnabled = true;
-            txtNombre.IsEnabled = true;
-            txtTelefono.IsEnabled = true;
-            txtEmail.IsEnabled = true;
+            txtAerolinea.IsEnabled = true;
+            txtOrigen.IsEnabled = true;
+            txtDestino.IsEnabled = true;
+            dpFecha.IsEnabled = true;
+            txtSalida.IsEnabled = true;
+            txtLlegada.IsEnabled = true;
+            txtAsientos.IsEnabled = true;
+            txtCosto.IsEnabled = true;
             btnGuardar.IsEnabled = true;
             Agregar_nuevo.IsEnabled = false;
             chkEliminar.IsEnabled = false;
@@ -131,10 +155,14 @@ namespace ProyectoPOE.Modulos
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             EstadoActual = Convert.ToInt32(EstadosVuelo.Eliminar);
-            txtDireccion.IsEnabled = false;
-            txtNombre.IsEnabled = false;
-            txtTelefono.IsEnabled = false;
-            txtEmail.IsEnabled = false;
+            txtAerolinea.IsEnabled = false;
+            txtOrigen.IsEnabled = false;
+            txtDestino.IsEnabled = false;
+            dpFecha.IsEnabled = false;
+            txtSalida.IsEnabled = false;
+            txtLlegada.IsEnabled = false;
+            txtAsientos.IsEnabled = false;
+            txtCosto.IsEnabled = false;
             btnGuardar.IsEnabled = true;
             Agregar_nuevo.IsEnabled = false;
         }
@@ -147,23 +175,33 @@ namespace ProyectoPOE.Modulos
         private void DGPasajero_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
             EstadoActual = Convert.ToInt32(EstadosVuelo.Modificar);
-            txtDireccion.IsEnabled = true;
-            txtNombre.IsEnabled = true;
-            txtTelefono.IsEnabled = true;
-            txtEmail.IsEnabled = true;
+            txtAerolinea.IsEnabled = true;
+            txtOrigen.IsEnabled = true;
+            txtDestino.IsEnabled = true;
+            dpFecha.IsEnabled = true;
+            txtSalida.IsEnabled = true;
+            txtLlegada.IsEnabled = true;
+            txtAsientos.IsEnabled = true;
+            txtCosto.IsEnabled = true;
             btnGuardar.IsEnabled = true;
             Agregar_nuevo.IsEnabled = false;
             chkEliminar.IsEnabled = true;
             chkEliminar.IsChecked = false;
         }
+
     }
 
     public class VueloViewModel
     {
         public int id { get; set; }
-        public string nombre { get; set; }
-        public string direccion { get; set; }
-        public string telefono { get; set; }
-        public string email { get; set; }
+        public string aerolinea { get; set; }
+        public string origen { get; set; }
+        public string destino { get; set; }
+        public DateTime? fecha { get; set; }
+        public TimeSpan? salida { get; set; }
+        public TimeSpan? llegada { get; set; }
+        public int? asientos { get; set;}
+        public double? costo { get; set;}
+
     }
 }
